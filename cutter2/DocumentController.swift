740a2106635da820f3b44015d3d268240a5735e8
Takashi Mochizuki <mochi@da2.so-net.ne.jp>
2019-04-06 11:26:17 +0900

    Happy new year.

--- cutter2/DocumentController.swift
+++ cutter2/DocumentController.swift
@@ -3,7 +3,7 @@
 //  cutter2
 //
 //  Created by Takashi Mochizuki on 2018/04/07.
-//  Copyright © 2018年 MyCometG3. All rights reserved.
+//  Copyright © 2018-2019年 MyCometG3. All rights reserved.
 //
 
 import Cocoa

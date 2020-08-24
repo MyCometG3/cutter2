976b12ae36bf1bb1f00e66cc65b708429db72e1e
Takashi Mochizuki <mochi@da2.so-net.ne.jp>
2020-08-24 20:14:12 +0900

    AppDelegate - Refactor/Cosmetics bookmark management.

--- cutter2/AppDelegate.swift
+++ cutter2/AppDelegate.swift
@@ -12,17 +12,19 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
     
     /* ============================================ */
-    // MARK: - Properties
+    // MARK: - Private properties/constants
     /* ============================================ */
     
-    private let bookmarksKey : String = "bookmarks"
+    private let bookmarksKey: String = "bookmarks"
+    
+    private var useLog: Bool = true
     
     /* ============================================ */
     // MARK: - NSApplicationDelegate protocol
     /* ============================================ */
     
     public func applicationDidFinishLaunching(_ aNotification: Notification) {
-        clearBookmarks()
+        clearBookmarks(false)
         startBookmarkAccess()
     }
     
@@ -44,7 +46,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBAction func nextDocument(_ sender: Any) {
         // Swift.print(#function, #line, #file)
         
-        let docList : [Document]? = NSApp.orderedDocuments as? [Document]
+        let docList: [Document]? = NSApp.orderedDocuments as? [Document]
         if let docList = docList, docList.count > 0 {
             if let doc = docList.last, let window = doc.window {
                 window.makeKeyAndOrderFront(self)
@@ -57,27 +59,27 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     /* ============================================ */
     
     /// Remove all bookmarks on startup
-    private func clearBookmarks() {
+    public func clearBookmarks(_ force: Bool) {
         // Swift.print(#function, #line, #file)
         
-        let needClear : Bool = NSEvent.modifierFlags.contains(.option)
+        let needClear: Bool = force ? true : NSEvent.modifierFlags.contains(.option)
         if needClear {
             let defaults = UserDefaults.standard
             defaults.set(nil, forKey: bookmarksKey)
             
-            Swift.print("NOTE: All bookmarks are removed.")
+            log("NOTE: All bookmarks are removed.")
         }
     }
     
     /// Register url as bookmark
     ///
     /// - Parameter newURL: url to register as bookmark
-    public func addBookmark(for newURL : URL) {
+    public func addBookmark(for newURL: URL) {
         // Swift.print(#function, #line, #file)
         
         // Check duplicate
-        var found : Bool = false
-        validateBookmarks(using: {(url) in
+        var found: Bool = false
+        validateBookmarks(false, using: {(url) in
             if url.path == newURL.path {
                 found = true
             }
@@ -87,21 +89,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
         
         // Register new bookmark
-        let data : Data? = try? newURL.bookmarkData(options: .withSecurityScope,
-                                                    includingResourceValuesForKeys: nil,
-                                                    relativeTo: nil)
-        if let data = data {
-            Swift.print("NOTE: Registered", newURL.path)
+        if let data = createBookmark(for: newURL) {
+            log("NOTE: Register bookmark -", newURL.path)
             
             let defaults = UserDefaults.standard
-            var bookmarks : [Data] = []
+            var bookmarks: [Data] = []
             if let array = defaults.array(forKey: bookmarksKey) {
                 bookmarks = array as! [Data]
             }
             bookmarks.append(data)
             defaults.set(bookmarks, forKey: bookmarksKey)
         } else {
-            Swift.print("NOTE: Invalid url", newURL.path)
+            log("NOTE: Invalid url -", newURL.path)
         }
     }
     
@@ -109,7 +108,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     private func startBookmarkAccess() {
         // Swift.print(#function, #line, #file)
         
-        validateBookmarks(using: {(url) in
+        validateBookmarks(true, using: {(url) in
             _ = url.startAccessingSecurityScopedResource()
         })
     }
@@ -118,7 +117,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     private func stopBookmarkAccess() {
         // Swift.print(#function, #line, #file)
         
-        validateBookmarks(using: {(url) in
+        validateBookmarks(true, using: {(url) in
             url.stopAccessingSecurityScopedResource()
         })
     }
@@ -126,36 +125,96 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     /// Validate bookmarks with block
     ///
     /// - Parameter task: block to process bookmark url
-    private func validateBookmarks(using task : ((URL) -> Void)) {
+    private func validateBookmarks(_ verbose: Bool, using task: ((URL) -> Void)) {
         // Swift.print(#function, #line, #file)
         
-        var validItems : [Data] = []
+        let useLogOriginal: Bool = useLog
+        useLog = verbose
+        var validItems: [Data] = []
         let defaults = UserDefaults.standard
         if let bookmarks = defaults.array(forKey: bookmarksKey) {
-            for item : Data in (bookmarks as! [Data]) {
-                // NOTE: Preserve staled bookmark for later use.
-                // NOTE: AVMovie does not use bookmark. Depends on filePath String.
-                // So staled media resource is inaccessible via reference movie. (as of 10.13.x)
-                do {
-                    var stale : Bool = false
-                    let url : URL? = try URL(resolvingBookmarkData: item,
-                                             options: .withSecurityScope,
-                                             relativeTo: nil,
-                                             bookmarkDataIsStale: &stale)
-                    if let url = url {
-                        Swift.print("NOTE: Valid bookmark -", url.path, (stale ? "staled" : ""))
-                        
-                        validItems.append(item)
-                        task(url)
-                    } else {
-                        Swift.print("NOTE: Invalid bookmark")
-                    }
-                } catch {
-                    Swift.print("NOTE: Invalid bookmark :", error.localizedDescription)
+            for item: Data in (bookmarks as! [Data]) {
+                /*
+                 Preserve souce movie file information as security scoped bookmark data.
+                 
+                 Restriction:
+                 Different from legacy QuickTime framework, AVMovie does not use bookmark/alias for
+                 sample reference. It depends on filepath string and doesn't follow file path change.
+                 */
+                var url: URL? = nil
+                let validated: Data? = refreshBookmarkIfRequired(item, urlOut: &url, acceptStale: false)
+                if let validated = validated, let url = url {
+                    validItems.append(validated)
+                    task(url)
                 }
             }
         }
         defaults.set(validItems, forKey: bookmarksKey)
+        useLog = useLogOriginal
+    }
+    
+    /// Validate bookmark data and refresh if required.
+    /// - Parameters:
+    ///   - item: bookmark data to be validated
+    ///   - urlOut: resolved url from the bookmark
+    ///   - acceptStale: accept stale bookmark or not
+    /// - Returns: resulted bookmark data
+    private func refreshBookmarkIfRequired(_ item: Data, urlOut: inout URL?, acceptStale: Bool) -> Data? {
+        // Swift.print(#function, #line, #file)
+        
+        // Validate bookmark item
+        do {
+            var stale: Bool = false
+            let url: URL = try URL(resolvingBookmarkData: item,
+                                   options: .withSecurityScope,
+                                   relativeTo: nil,
+                                   bookmarkDataIsStale: &stale)
+            urlOut = url
+            if !stale {
+                log("NOTE: Valid bookmark -", url.path)
+                return item // Valid bookmark - No change
+            } else {
+                // Try renewing bookmark item
+                if let newItem = createBookmark(for: url) {
+                    log("NOTE: Valid bookmark -", url.path, "(renewed)")
+                    return newItem // Renewed bookmark
+                }
+                
+                // Failed to create new bookmark for the url
+                if acceptStale {
+                    log("NOTE: Valid bookmark -", url.path, (stale ? "(stale)" : ""))
+                    return item // Staled bookmark - No change
+                } else {
+                    log("NOTE: Invalidate bookmark -", url.path, (stale ? "(stale)" : ""))
+                    return nil // Staled => Invalid
+                }
+            }
+        } catch {
+            log("NOTE: Invalid bookmark -", error.localizedDescription)
+            return nil // Invalid bookmark - nil returned
+        }
+    }
+    
+    /// Create security scoped bookmark data from the url
+    /// - Parameter url: source url
+    /// - Returns: resulted bookmark data
+    private func createBookmark(for url: URL) -> Data? {
+        // Swift.print(#function, #line, #file)
+        
+        let data: Data? = try? url.bookmarkData(options: .withSecurityScope,
+                                                includingResourceValuesForKeys: nil,
+                                                relativeTo: nil)
+        return data
+    }
+    
+    /// Debug logging bookmark validation activity when useLog == true.
+    /// - Parameter items: bookmark validation strings to be logged
+    private func log(_ items: Any...) {
+        // Swift.print(#function, #line, #file)
+        
+        if useLog {
+            let output = items.map{ ($0 as AnyObject).description }.joined(separator:" ")
+            Swift.print(output)
+        }
     }
 }
-

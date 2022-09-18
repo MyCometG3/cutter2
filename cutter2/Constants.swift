a98f08b481deb14efbed83e66f79232e6dde4716
Takashi Mochizuki <mycometg3@icloud.com>
2022-09-18 19:39:44 +0900

    It's 2022.

--- cutter2/Constants.swift
+++ cutter2/Constants.swift
@@ -3,7 +3,7 @@
 //  cutter2
 //
 //  Created by Takashi Mochizuki on 2020/08/14.
-//  Copyright © 2021 MyCometG3. All rights reserved.
+//  Copyright © 2022 MyCometG3. All rights reserved.
 //
 
 import Foundation

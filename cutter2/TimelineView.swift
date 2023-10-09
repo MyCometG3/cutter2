1af2a8b32a135df5bc20acdcd2e54b3cac54a6f6
Takashi Mochizuki <mycometg3@icloud.com>
2023-10-09 16:26:04 +0900

    Avoid warning about NSBezierPath.ElementType.

--- cutter2/TimelineView.swift
+++ cutter2/TimelineView.swift
@@ -25,8 +25,12 @@ public extension NSBezierPath {
                 path.addCurve(to: points[2], control1: points[0], control2: points[1])
             case .closePath:
                 path.closeSubpath()
+            case .cubicCurveTo:
+                path.addCurve(to: points[2], control1: points[0], control2: points[1])
+            case .quadraticCurveTo:
+                path.addQuadCurve(to: points[1], control: points[0])
             @unknown default:
-                break
+                fatalError()
             }
         }
         return path
